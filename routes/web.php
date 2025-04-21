4d3bb92b7959a4eda48f186031031131c2cba9c2
Paolo Nikko Nunal <paolo.nunal24@gmail.com>
2025-04-22 11:22:35 +1200

    Added test call for package

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Drake24\Lynx\Lynx;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -13,5 +14,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+Route::get('test', function () {
+    info('test');
+    Lynx::connect();
+});
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
