7ca26fe1fa376cb785054dbe7e7b4e9ac0a7aa0d
Paolo Nikko Nunal <paolo.nunal24@gmail.com>
2025-04-21 13:32:38 +1200

    Added package files

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,17 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get('/lynx', function () {
+    $lynx = app(\Drake24\Lynx::class);
+    return $lynx->connect();
+});
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
+        info('test');
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
